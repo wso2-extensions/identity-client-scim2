70edbec377fd91c5c974e3a2e4fa965c7110eac8
Isuranga Perera <isurangamperera@gmail.com>
2017-11-04 21:27:36 +0530

    update worker

--- src/main/java/org/wso2/scim2/client/Worker.java
+++ src/main/java/org/wso2/scim2/client/Worker.java
@@ -1,45 +1,60 @@
 package org.wso2.scim2.client;
 
+import io.swagger.client.ApiException;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.wso2.charon3.core.encoder.JSONDecoder;
 import org.wso2.charon3.core.exceptions.BadRequestException;
 import org.wso2.charon3.core.exceptions.CharonException;
 import org.wso2.charon3.core.exceptions.InternalErrorException;
+import org.wso2.charon3.core.exceptions.NotFoundException;
 import org.wso2.charon3.core.objects.User;
+import org.wso2.charon3.core.schema.SCIMConstants.UserSchemaConstants;
 import org.wso2.charon3.core.schema.SCIMResourceSchemaManager;
 import org.wso2.charon3.core.schema.SCIMResourceTypeSchema;
-import org.wso2.charon3.core.schema.SCIMConstants.UserSchemaConstants;
 import org.wso2.scim2.exception.IdentitySCIMException;
+import org.wso2.scim2.operation.UserOperation;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Worker {
 
-	public static String DEFINED_USER = "{\"externalID\":\"abergin2\",\"displayName\":\"Andy Bergin\",\"userName\":\"black1300\",\"password\":\"admin\",\"name\":{\"givenName\":\"Andy\",\"familyName\":\"Bergin\"},\"phoneNumbers\":[{\"type\":\"work\",\"value\":\"+1 408 555 8585\"},{\"type\":\"fax\",\"value\":\"+1 408 555 7472\"}],\"emails\":[{\"type\":\"work\",\"value\":\"abergin@example.com\"}]}";
+    public static String DEFINED_USER = "{\"userName\":\"black_thunder3\",\"name\":{\"givenName\":\"Andy\",\"familyName\":\"Gonja\"},\"emails\":[{\"type\":\"work\",\"value\":\"abergin@example.com\"}]}";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws InternalErrorException, BadRequestException, CharonException, IdentitySCIMException, ApiException, NotFoundException, IOException {
+        Logger.getRootLogger().setLevel(Level.INFO);
+        //BasicConfigurator.configure();
 
-		SCIMProvider scim = new SCIMProvider();
-		scim.setProperty(UserSchemaConstants.USER_NAME, "admin");
-		scim.setProperty(UserSchemaConstants.PASSWORD, "admin");
+        SCIMProvider scim = new SCIMProvider();
+        scim.setProperty(UserSchemaConstants.USER_NAME, "admin");
+        scim.setProperty(UserSchemaConstants.PASSWORD, "admin");
 
-		SCIMResourceTypeSchema schema = SCIMResourceSchemaManager.getInstance()
-				.getUserResourceSchema();
+        SCIMResourceTypeSchema schema = SCIMResourceSchemaManager.getInstance()
+                .getUserResourceSchema();
 
-		JSONDecoder jsonDecoder = new JSONDecoder();
-		User user = null;
+        JSONDecoder jsonDecoder = new JSONDecoder();
+        User user = null;
 
-		try {
-			user = (User) jsonDecoder.decodeResource(DEFINED_USER, schema,
-					new User());
-		} catch (BadRequestException | CharonException | InternalErrorException e) {
-			
-		}
+        user = (User) jsonDecoder.decodeResource(DEFINED_USER, schema,
+                new User());
 
-		ProvisioningClient client = new ProvisioningClient(scim, user, 1, null);
+        ProvisioningClient client = new ProvisioningClient(scim, user, null);
 
-		try {
-			client.provisionCreateUser();
-		} catch (IdentitySCIMException e) {
-			
-		}
-	}
+        //user = client.provisionCreateUser();
+
+        UserOperation o = new UserOperation(scim, user,null);
+        //o.provisionCreateUser();
+        List<User> users = o.listWithGet(null, null, null,0,5,null, null);
+
+        client.provisionUpdateUser();
+
+        for(int i = 0; i < users.size(); i++) {
+            System.out.print(users.get(i).getUserName()+"   ");
+            System.out.println(users.get(i).getLastModified());
+        }
+
+    }
 
 }
