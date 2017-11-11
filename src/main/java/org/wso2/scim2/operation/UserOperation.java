971b79000f7cddf58380bfba96a6aa8fb2050123
Isuranga Perera <isurangamperera@gmail.com>
2017-11-11 09:48:13 +0530

    add patch operation

--- src/main/java/org/wso2/scim2/operation/UserOperation.java
+++ src/main/java/org/wso2/scim2/operation/UserOperation.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.wso2.scim2.operation;
 
@@ -24,6 +24,7 @@ import io.swagger.client.api.Scimv2UsersApi;
 import java.io.IOException;
 import java.util.*;
 
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.charon3.core.config.CharonConfiguration;
@@ -48,6 +49,7 @@ import org.wso2.scim2.model.Error;
 import org.wso2.scim2.model.UserList;
 
 import com.google.gson.Gson;
+import org.wso2.scim2.util.PatchOperationEncoder;
 
 public class UserOperation {
 
@@ -267,6 +269,7 @@ public class UserOperation {
                 encodedObject = new JSONEncoder().encodeSCIMObject(scimObject);
             } else if(httpMethod.equals("PATCH")) {
                 List<PatchOperation> patchOperations = provider.getPatchOperationList();
+                encodedObject = new PatchOperationEncoder().encodeRequest(patchOperations);
             } else {
                 logger.error("Not supported update operation type: " + httpMethod);
             }
@@ -304,6 +307,8 @@ public class UserOperation {
             throw new IdentitySCIMException("No resulted users found in the user store.", e);
         } catch (IOException e) {
             throw new IdentitySCIMException(e.getMessage(), e);
+        } catch (JSONException e) {
+            throw new IdentitySCIMException("Error while encoding patch operations");
         }
 
         return updatedUser;
