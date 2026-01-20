ab1d5791f5bc3fd7283f483392333682a26892ba
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2026-01-21 00:08:59 +0530

    Provide support for SCIM Patch operation for User

--- src/main/java/org/wso2/scim2/client/ProvisioningClient.java
+++ src/main/java/org/wso2/scim2/client/ProvisioningClient.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018-2026, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.scim2.client;
@@ -92,6 +94,21 @@ public class ProvisioningClient {
             throw new IdentitySCIMException("Unable to update the User.", e);
         } catch (IdentitySCIMException e) {
             throw new IdentitySCIMException("Unable to update the User.", e);
+        }
+    }
+
+    /**
+     * Provision the patch user.
+     * @throws IdentitySCIMException if an error occurs while patching the user.
+     */
+    public void provisionPatchUser() throws IdentitySCIMException {
+
+        UserOperations operation;
+        try {
+            operation = new UserOperations(provider, scimObject, additionalProvisioningInformation);
+            operation.patchUser();
+        } catch (ScimApiException | IdentitySCIMException e) {
+            throw new IdentitySCIMException("Unable to patch the User.", e);
         }
     }
 
